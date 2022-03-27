446be59c50a968c76726ba8ea60c8dadc1251a42
capmahin <mahinulhuque1996@gmail.com>
2022-03-28 03:36:55 +0600

    add card and card group and distraturing

--- src/components/CardGroup/CardGroup.js
+++ src/components/CardGroup/CardGroup.js
@@ -1,9 +1,23 @@
 import React from 'react';
+import Card from '../Card/Card';
 
 const CardGroup = () => {
+    const products = [
+        {id:1 ,name:'Laptop', price:12500},
+        {id:2 ,name:'Laptop pro', price:12500},
+        {id:3 ,name:'Laptop legand', price:12500}
+    ]
     return (
         <div>
             <h2>This is my cards</h2>
+            <div className="card-group">
+                {
+                    products.map(product => <Card 
+                        key={product.id} 
+                        product ={product}
+                        ></Card>)
+                }
+</div>
         </div>
     );
 };
